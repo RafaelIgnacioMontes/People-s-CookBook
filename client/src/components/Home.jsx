e414b3b56cbf25dca95fb177c32b9a16a9cdf10d
RafaelIgnacioMontes <rafaellending@gmail.com>
2023-02-16 07:20:50 -0700

    Added Styling

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,12 +1,12 @@
 const Home = () => {
   return (
     <div className="home">
-      <div className="headers">
-        <h1>People's Cook Book</h1>
-        <h2>
-          Welcome to the cook book of the world, where everyone's recipes are
+      <div className="mainmenu">
+        People's Cook Book
+        <p className="pepu">
+          Welcome to the cook book of the world, where everyone's recipes is
           everyone's.
-        </h2>
+        </p>
       </div>
     </div>
   )
