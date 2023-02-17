f509940466abdf49de7752c370306c28ca3d5e2c
RafaelIgnacioMontes <rafaellending@gmail.com>
2023-02-17 07:21:53 -0700

    Added image to home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -8,6 +8,10 @@ const Home = () => {
           Welcome to the cook book of the world, where everyone's recipes is
           everyone's.
         </p>
+        <img
+          src="https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=749&q=80"
+          alt="french toast"
+        />
       </div>
     </div>
   )
